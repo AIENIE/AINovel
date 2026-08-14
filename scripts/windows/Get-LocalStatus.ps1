608ffb053bee5a27d7ab55c8010bc3a35b026e69
duwei <duwei@aienie.com>
2026-08-14 13:44:20 +0800

    ops: add native PowerShell lifecycle wrappers

---
+++ scripts/windows/Get-LocalStatus.ps1
@@ -0,0 +1,5 @@
+[CmdletBinding()]
+param([switch]$AsJson)
+
+& (Join-Path $PSScriptRoot 'Invoke-Local.ps1') -Action Status -AsJson:$AsJson
+exit $LASTEXITCODE
---
+++ scripts/windows/Test-Local.ps1
@@ -0,0 +1,8 @@
+[CmdletBinding()]
+param(
+    [ValidateSet('All', 'Backend', 'Frontend')][string]$Component = 'All',
+    [ValidateSet('L1', 'L2')][string]$Level = 'L2'
+)
+
+& (Join-Path $PSScriptRoot 'Invoke-Local.ps1') -Action Test -Component $Component -TestLevel $Level
+exit $LASTEXITCODE
